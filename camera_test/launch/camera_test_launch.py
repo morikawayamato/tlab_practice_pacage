9b3fdff3253038405f8119790e3c861350eebe03
MoriMori <ukato.nocobor@gmail.com>
2025-09-02 13:21:47 +0900

    rqt_image_view同時に起動できるように実装

--- camera_test/launch/camera_test_launch.py
+++ camera_test/launch/camera_test_launch.py
@@ -7,13 +7,20 @@ def generate_launch_description():
     ld.add_action(Node(
         package = 'v4l2_camera',
         executable = 'v4l2_camera_node',
-        output = 'screen'
-    ))
+        parameters=[{
+                'video_device': '/dev/video0' # 使用するカメラデバイス
+                # 'image_width': 640,
+                # 'image_height': 480,
+                # 'frame_rate': 30.0,
+            }]
+    )),
 
-    # ld.add_action(Node(
-    #     package = 'rqt_image_view',
-    #     name ='rqt_image_view',
-    #     executable = 'image_view'
-    # ))
+    ld.add_action(Node(
+        package = 'rqt_image_view',
+        name ='rqt_image_view',
+        executable = 'rqt_image_view',
+        output = 'screen',
+        # arguments=['/image'] 
+    ))
 
     return ld
